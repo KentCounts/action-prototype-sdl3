9fdb60179e25ce41187705a5419085c515c378f4
KentCounts <77399771+KentCounts@users.noreply.github.com>
2026-01-21 18:35:51 -0600

    outline for leaderboard .h and .cpp for later implementation

---
+++ action-prototype-sdl3/Leaderboard.cpp
@@ -0,0 +1,20 @@
+#include "Leaderboard.h"
+
+// constructor
+
+// load
+
+// save
+
+// add score
+//	push to vector
+//	sort
+//	trim to top 10?
+
+// get leaderboard
+
+// display leaderboard
+
+// clear
+
+// sort
---
+++ action-prototype-sdl3/Leaderboard.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <string>
+#include <vector>
+
+// dat astruct for leaderboard entry goes here
+
+// class for leaderboard
+// load
+// save
+// add score
+// display high scores
+// clear
+
+// vector for storing leader boards
+// const for top 10?
+// sort?
